9cc664c3756ec8f50d4f495ff95b0ffd7e39a607
LUCAS <lucarty79@gmail.com>
2026-06-19 15:09:00 -0300

    Refatora MaterialService e adiciona novos métodos
    Renomeia o método `GetAllSync` para `GetAllAsync` para refletir uma abordagem assíncrona.
    Adiciona os métodos `CreateAsync`, `UpdateAsync`, `DeleteAsync` e `CountAsync` para gerenciar materiais.
    Inclui o método privado `MapToDto` para conversão de objetos `Material` em `MaterialDto`.
    Essas mudanças melhoram a funcionalidade e a consistência do serviço.

--- AtelieDosPontinhos.Application/Services/MaterialService.cs
+++ AtelieDosPontinhos.Application/Services/MaterialService.cs
@@ -19,7 +19,7 @@ namespace AtelieDosPontinhos.Application.Services
             _materialRepository = materialRepository;
         }
 
-        public async Task<IEnumerable<MaterialDto>> GetAllSync()
+        public async Task<IEnumerable<MaterialDto>> GetAllAsync()
         {
             var material = await _materialRepository.GetAllAsync();
             return material.Select(MapToDto);
@@ -30,6 +30,47 @@ namespace AtelieDosPontinhos.Application.Services
             var material = await _materialRepository.GetByIdAsync(id);
             return material == null ? null : MapToDto(material);
         }
-        public async
+        public async Task<MaterialDto> CreateAsync (CreateMaterialDto dto)
+        {
+            var material = new Material { Name = dto.Name,
+            Unit = dto.Unit,
+            Amount = dto.Amount};
+            await _materialRepository.AddAsync(material);
+            return MapToDto(material);
+        }
+        public async Task<MaterialDto?> UpdateAsync(int id, UpdateMaterialDto dto)
+        {
+            var material = await _materialRepository.GetByIdAsync(id);
+            if (material == null) return null;
+
+            material.Name = dto.Name;
+            material.Unit = dto.Unit;
+            material.Amount = dto.Amount;
+            await _materialRepository.UpdateAsync(material);
+            return MapToDto(material);
+            
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var material = _materialRepository.GetByIdAsync(id);
+            if (material == null) return false;
+
+            await _materialRepository.DeleteAsync(id);
+            return true;
+        }
+        public async Task<int> CountAsync()
+        {
+            return await _materialRepository.CountAsync();
+        }
+        private static MaterialDto MapToDto(Material material)
+        {
+            return new MaterialDto
+            {
+                Id = material.Id,
+                Name = material.Name,
+                Amount = material.Amount,
+                Unit = material.Unit,
+            };
+        }
     }
 }
