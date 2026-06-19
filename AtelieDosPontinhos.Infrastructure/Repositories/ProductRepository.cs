7326d2c8edd069fcdf73c9137c03d7614726c9ae
Jessica <jessicagavazzi2006@gmail.com>
2026-06-19 17:17:53 -0300

    Refatoração: remoção de Product_Material e ajustes
    Removidos arquivos relacionados à entidade Product_Material:
    - `Product_MaterialDto.cs` (DTOs: Product_MaterialDto, CreateProduct_MaterialDto, UpdateProduct_MaterialDto).
    - `IProduct_MaterialService.cs` (interface para operações de Product_Material).
    - `Product_MaterialRepository.cs` (classe vazia).
    
    Alterações no `ProductRepository.cs`:
    - Tornada a classe pública e adicionado suporte ao contexto `AtelieDosPontinhosDbContext`.
    - Implementado o método `GetAllAsync` com JOIN na tabela de categorias e ordenação por preço decrescente.
    
    Essas mudanças indicam uma descontinuação ou reestruturação da funcionalidade de Product_Material e melhorias na organização do repositório de produtos.

--- AtelieDosPontinhos.Infrastructure/Repositories/ProductRepository.cs
+++ AtelieDosPontinhos.Infrastructure/Repositories/ProductRepository.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using AtelieDosPontinhos.Domain.Entities;
+using AtelieDosPontinhos.Domain.Interfaces;
+using AtelieDosPontinhos.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AtelieDosPontinhos.Infrastructure.Repositories
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly AtelieDosPontinhosDbContext _context;
+
+        public ProductRepository(AtelieDosPontinhosDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products
+                .Include(p => p.Category) // Faz JOIN com a tabela Categoria
+                .OrderByDescending(p => p.Price) // Ordenar por preço mais caro 
+                .ToListAsync();
+        }
+
+
     }
 }
