d3e5be235a336e4811fb514c49f0ae2929368c47
LUCAS <lucarty79@gmail.com>
2026-08-17 17:09:21 -0300

    pppo

---
+++ AtelieDosPontinhos.Desktop/DTOs/AuthDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtelieDosPontinhos.Desktop.DTOs
+{
+    public class loginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+
+    public class RegisterDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+    public class UserDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public IList<string> Roles { get; set; } = new List<string>();
+        public bool IsAdmin => Roles.Contains("Admin");
+    }
+}
---
+++ AtelieDosPontinhos.Desktop/DTOs/CategoryDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtelieDosPontinhos.Desktop.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int ProductCount { get; set; }
+
+        public string ImageLocal { get; set; } = string.Empty;
+    }
+    //criar categoria
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string ImageLocal { get; set; } = string.Empty;
+    }
+    //atualiza a categoria
+    public class UpdateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string ImageLocal { get; set; } = string.Empty;
+    }
+}
