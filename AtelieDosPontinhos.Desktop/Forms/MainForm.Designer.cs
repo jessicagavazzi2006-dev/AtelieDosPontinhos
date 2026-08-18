42b72ea1280d26d77f7b3bf5a3f22a0a37daf9e3
LUCAS <lucarty79@gmail.com>
2026-08-18 16:15:45 -0300

    hj 16 15

--- AtelieDosPontinhos.Desktop/Forms/MainForm.Designer.cs
+++ AtelieDosPontinhos.Desktop/Forms/MainForm.Designer.cs
@@ -28,35 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             pnlUsuario = new Panel();
-            pnlHeader = new Panel();
-            pnlLogo = new Panel();
-            pnlSidebar = new Panel();
-            pnlConteudo = new Panel();
-            lblUsuario = new Label();
             lblPerfil = new Label();
+            lblUsuario = new Label();
+            pnlHeader = new Panel();
+            btnLogout = new Guna.UI2.WinForms.Guna2Button();
             lblTituloApp = new Label();
+            pnlLogo = new Panel();
             lblSidebarLogo = new Label();
             lblSidebarSub = new Label();
-            lblSessao = new Label();
-            btnLogout = new Guna.UI2.WinForms.Guna2Button();
-            btnDashboard = new Guna.UI2.WinForms.Guna2Button();
-            btnProdutos = new Guna.UI2.WinForms.Guna2Button();
-            btnCategorias = new Guna.UI2.WinForms.Guna2Button();
-            btnUsuarios = new Guna.UI2.WinForms.Guna2Button();
+            pnlSidebar = new Panel();
             btnPerfil = new Guna.UI2.WinForms.Guna2Button();
+            btnUsuarios = new Guna.UI2.WinForms.Guna2Button();
+            btnCategorias = new Guna.UI2.WinForms.Guna2Button();
+            btnProdutos = new Guna.UI2.WinForms.Guna2Button();
+            btnDashboard = new Guna.UI2.WinForms.Guna2Button();
+            lblSessao = new Label();
+            pnlConteudo = new Panel();
             pnlUsuario.SuspendLayout();
             pnlHeader.SuspendLayout();
             pnlLogo.SuspendLayout();
@@ -72,43 +72,14 @@
             pnlUsuario.Size = new Size(200, 100);
             pnlUsuario.TabIndex = 0;
             // 
-            // pnlHeader
+            // lblPerfil
             // 
-            pnlHeader.Controls.Add(btnLogout);
-            pnlHeader.Controls.Add(lblTituloApp);
-            pnlHeader.Location = new Point(201, 0);
-            pnlHeader.Name = "pnlHeader";
-            pnlHeader.Size = new Size(823, 100);
-            pnlHeader.TabIndex = 0;
-            // 
-            // pnlLogo
-            // 
-            pnlLogo.Controls.Add(lblSidebarLogo);
-            pnlLogo.Controls.Add(lblSidebarSub);
-            pnlLogo.Location = new Point(0, 101);
-            pnlLogo.Name = "pnlLogo";
-            pnlLogo.Size = new Size(200, 73);
-            pnlLogo.TabIndex = 0;
-            // 
-            // pnlSidebar
-            // 
-            pnlSidebar.Controls.Add(btnPerfil);
-            pnlSidebar.Controls.Add(btnUsuarios);
-            pnlSidebar.Controls.Add(btnCategorias);
-            pnlSidebar.Controls.Add(btnProdutos);
-            pnlSidebar.Controls.Add(btnDashboard);
-            pnlSidebar.Controls.Add(lblSessao);
-            pnlSidebar.Location = new Point(0, 175);
-            pnlSidebar.Name = "pnlSidebar";
-            pnlSidebar.Size = new Size(200, 445);
-            pnlSidebar.TabIndex = 0;
-            // 
-            // pnlConteudo
-            // 
-            pnlConteudo.Location = new Point(208, 109);
-            pnlConteudo.Name = "pnlConteudo";
-            pnlConteudo.Size = new Size(805, 501);
-            pnlConteudo.TabIndex = 0;
+            lblPerfil.AutoSize = true;
+            lblPerfil.Location = new Point(22, 54);
+            lblPerfil.Name = "lblPerfil";
+            lblPerfil.Size = new Size(34, 15);
+            lblPerfil.TabIndex = 0;
+            lblPerfil.Text = "Perfil";
             // 
             // lblUsuario
             // 
@@ -119,14 +90,30 @@
             lblUsuario.TabIndex = 0;
             lblUsuario.Text = "👤 Usuário";
             // 
-            // lblPerfil
+            // pnlHeader
             // 
-            lblPerfil.AutoSize = true;
-            lblPerfil.Location = new Point(22, 54);
-            lblPerfil.Name = "lblPerfil";
-            lblPerfil.Size = new Size(34, 15);
-            lblPerfil.TabIndex = 0;
-            lblPerfil.Text = "Perfil";
+            pnlHeader.Controls.Add(btnLogout);
+            pnlHeader.Controls.Add(lblTituloApp);
+            pnlHeader.Location = new Point(201, 0);
+            pnlHeader.Name = "pnlHeader";
+            pnlHeader.Size = new Size(823, 100);
+            pnlHeader.TabIndex = 0;
+            // 
+            // btnLogout
+            // 
+            btnLogout.CustomizableEdges = customizableEdges1;
+            btnLogout.DisabledState.BorderColor = Color.DarkGray;
+            btnLogout.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnLogout.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnLogout.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnLogout.Font = new Font("Segoe UI", 9F);
+            btnLogout.ForeColor = Color.White;
+            btnLogout.Location = new Point(612, 23);
+            btnLogout.Name = "btnLogout";
+            btnLogout.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btnLogout.Size = new Size(180, 45);
+            btnLogout.TabIndex = 0;
+            btnLogout.Text = "Sair";
             // 
             // lblTituloApp
             // 
@@ -136,6 +123,15 @@
             lblTituloApp.Size = new Size(116, 15);
             lblTituloApp.TabIndex = 0;
             lblTituloApp.Text = "Ateliê dos Pontinhos";
+            // 
+            // pnlLogo
+            // 
+            pnlLogo.Controls.Add(lblSidebarLogo);
+            pnlLogo.Controls.Add(lblSidebarSub);
+            pnlLogo.Location = new Point(0, 101);
+            pnlLogo.Name = "pnlLogo";
+            pnlLogo.Size = new Size(200, 73);
+            pnlLogo.TabIndex = 0;
             // 
             // lblSidebarLogo
             // 
@@ -155,6 +151,104 @@
             lblSidebarSub.TabIndex = 0;
             lblSidebarSub.Text = "Plataforma Desktop";
             // 
+            // pnlSidebar
+            // 
+            pnlSidebar.Controls.Add(btnPerfil);
+            pnlSidebar.Controls.Add(btnUsuarios);
+            pnlSidebar.Controls.Add(btnCategorias);
+            pnlSidebar.Controls.Add(btnProdutos);
+            pnlSidebar.Controls.Add(btnDashboard);
+            pnlSidebar.Controls.Add(lblSessao);
+            pnlSidebar.Location = new Point(0, 175);
+            pnlSidebar.Name = "pnlSidebar";
+            pnlSidebar.Size = new Size(200, 445);
+            pnlSidebar.TabIndex = 0;
+            // 
+            // btnPerfil
+            // 
+            btnPerfil.CustomizableEdges = customizableEdges3;
+            btnPerfil.DisabledState.BorderColor = Color.DarkGray;
+            btnPerfil.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnPerfil.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnPerfil.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnPerfil.Font = new Font("Segoe UI", 9F);
+            btnPerfil.ForeColor = Color.White;
+            btnPerfil.Location = new Point(0, 180);
+            btnPerfil.Name = "btnPerfil";
+            btnPerfil.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btnPerfil.Size = new Size(200, 45);
+            btnPerfil.TabIndex = 0;
+            btnPerfil.Text = "Meu Perfil";
+            btnPerfil.Click += btnPerfil_Click;
+            // 
+            // btnUsuarios
+            // 
+            btnUsuarios.CustomizableEdges = customizableEdges5;
+            btnUsuarios.DisabledState.BorderColor = Color.DarkGray;
+            btnUsuarios.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnUsuarios.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnUsuarios.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnUsuarios.Font = new Font("Segoe UI", 9F);
+            btnUsuarios.ForeColor = Color.White;
+            btnUsuarios.Location = new Point(0, 135);
+            btnUsuarios.Name = "btnUsuarios";
+            btnUsuarios.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            btnUsuarios.Size = new Size(200, 45);
+            btnUsuarios.TabIndex = 0;
+            btnUsuarios.Text = "Usuários";
+            btnUsuarios.Click += btnUsuarios_Click;
+            // 
+            // btnCategorias
+            // 
+            btnCategorias.CustomizableEdges = customizableEdges7;
+            btnCategorias.DisabledState.BorderColor = Color.DarkGray;
+            btnCategorias.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnCategorias.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnCategorias.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnCategorias.Font = new Font("Segoe UI", 9F);
+            btnCategorias.ForeColor = Color.White;
+            btnCategorias.Location = new Point(0, 90);
+            btnCategorias.Name = "btnCategorias";
+            btnCategorias.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btnCategorias.Size = new Size(200, 45);
+            btnCategorias.TabIndex = 0;
+            btnCategorias.Text = "Categorias";
+            btnCategorias.Click += btnCategorias_Click;
+            // 
+            // btnProdutos
+            // 
+            btnProdutos.CustomizableEdges = customizableEdges9;
+            btnProdutos.DisabledState.BorderColor = Color.DarkGray;
+            btnProdutos.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnProdutos.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnProdutos.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnProdutos.Font = new Font("Segoe UI", 9F);
+            btnProdutos.ForeColor = Color.White;
+            btnProdutos.Location = new Point(0, 45);
+            btnProdutos.Name = "btnProdutos";
+            btnProdutos.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            btnProdutos.Size = new Size(200, 45);
+            btnProdutos.TabIndex = 0;
+            btnProdutos.Text = "Produtos";
+            btnProdutos.Click += btnProdutos_Click;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.CustomizableEdges = customizableEdges11;
+            btnDashboard.DisabledState.BorderColor = Color.DarkGray;
+            btnDashboard.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnDashboard.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnDashboard.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnDashboard.Font = new Font("Segoe UI", 9F);
+            btnDashboard.ForeColor = Color.White;
+            btnDashboard.Location = new Point(0, 0);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            btnDashboard.Size = new Size(200, 45);
+            btnDashboard.TabIndex = 0;
+            btnDashboard.Text = "Dashboard";
+            btnDashboard.Click += btnDashboard_Click_1;
+            // 
             // lblSessao
             // 
             lblSessao.AutoSize = true;
@@ -164,101 +258,12 @@
             lblSessao.TabIndex = 0;
             lblSessao.Text = "...";
             // 
-            // btnLogout
+            // pnlConteudo
             // 
-            btnLogout.CustomizableEdges = customizableEdges13;
-            btnLogout.DisabledState.BorderColor = Color.DarkGray;
-            btnLogout.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnLogout.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnLogout.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnLogout.Font = new Font("Segoe UI", 9F);
-            btnLogout.ForeColor = Color.White;
-            btnLogout.Location = new Point(612, 23);
-            btnLogout.Name = "btnLogout";
-            btnLogout.ShadowDecoration.CustomizableEdges = customizableEdges14;
-            btnLogout.Size = new Size(180, 45);
-            btnLogout.TabIndex = 0;
-            btnLogout.Text = "Sair";
-            // 
-            // btnDashboard
-            // 
-            btnDashboard.CustomizableEdges = customizableEdges15;
-            btnDashboard.DisabledState.BorderColor = Color.DarkGray;
-            btnDashboard.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnDashboard.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnDashboard.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnDashboard.Font = new Font("Segoe UI", 9F);
-            btnDashboard.ForeColor = Color.White;
-            btnDashboard.Location = new Point(0, 0);
-            btnDashboard.Name = "btnDashboard";
-            btnDashboard.ShadowDecoration.CustomizableEdges = customizableEdges16;
-            btnDashboard.Size = new Size(200, 45);
-            btnDashboard.TabIndex = 0;
-            btnDashboard.Text = "Dashboard";
-            // 
-            // btnProdutos
-            // 
-            btnProdutos.CustomizableEdges = customizableEdges17;
-            btnProdutos.DisabledState.BorderColor = Color.DarkGray;
-            btnProdutos.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnProdutos.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnProdutos.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnProdutos.Font = new Font("Segoe UI", 9F);
-            btnProdutos.ForeColor = Color.White;
-            btnProdutos.Location = new Point(0, 45);
-            btnProdutos.Name = "btnProdutos";
-            btnProdutos.ShadowDecoration.CustomizableEdges = customizableEdges18;
-            btnProdutos.Size = new Size(200, 45);
-            btnProdutos.TabIndex = 0;
-            btnProdutos.Text = "Produtos";
-            // 
-            // btnCategorias
-            // 
-            btnCategorias.CustomizableEdges = customizableEdges19;
-            btnCategorias.DisabledState.BorderColor = Color.DarkGray;
-            btnCategorias.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnCategorias.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnCategorias.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnCategorias.Font = new Font("Segoe UI", 9F);
-            btnCategorias.ForeColor = Color.White;
-            btnCategorias.Location = new Point(0, 90);
-            btnCategorias.Name = "btnCategorias";
-            btnCategorias.ShadowDecoration.CustomizableEdges = customizableEdges20;
-            btnCategorias.Size = new Size(200, 45);
-            btnCategorias.TabIndex = 0;
-            btnCategorias.Text = "Categorias";
-            // 
-            // btnUsuarios
-            // 
-            btnUsuarios.CustomizableEdges = customizableEdges21;
-            btnUsuarios.DisabledState.BorderColor = Color.DarkGray;
-            btnUsuarios.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnUsuarios.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnUsuarios.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnUsuarios.Font = new Font("Segoe UI", 9F);
-            btnUsuarios.ForeColor = Color.White;
-            btnUsuarios.Location = new Point(0, 135);
-            btnUsuarios.Name = "btnUsuarios";
-            btnUsuarios.ShadowDecoration.CustomizableEdges = customizableEdges22;
-            btnUsuarios.Size = new Size(200, 45);
-            btnUsuarios.TabIndex = 0;
-            btnUsuarios.Text = "Usuários";
-            // 
-            // btnPerfil
-            // 
-            btnPerfil.CustomizableEdges = customizableEdges23;
-            btnPerfil.DisabledState.BorderColor = Color.DarkGray;
-            btnPerfil.DisabledState.CustomBorderColor = Color.DarkGray;
-            btnPerfil.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            btnPerfil.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            btnPerfil.Font = new Font("Segoe UI", 9F);
-            btnPerfil.ForeColor = Color.White;
-            btnPerfil.Location = new Point(0, 180);
-            btnPerfil.Name = "btnPerfil";
-            btnPerfil.ShadowDecoration.CustomizableEdges = customizableEdges24;
-            btnPerfil.Size = new Size(200, 45);
-            btnPerfil.TabIndex = 0;
-            btnPerfil.Text = "Meu Perfil";
+            pnlConteudo.Location = new Point(208, 109);
+            pnlConteudo.Name = "pnlConteudo";
+            pnlConteudo.Size = new Size(805, 501);
+            pnlConteudo.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -274,6 +279,7 @@
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainForm";
+            Load += MainForm_Load;
             pnlUsuario.ResumeLayout(false);
             pnlUsuario.PerformLayout();
             pnlHeader.ResumeLayout(false);
