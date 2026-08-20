b8ca248960669e7a21e249632bb9be5992f683f3
LUCAS <lucarty79@gmail.com>
2026-08-20 17:06:07 -0300

    ´´´´´´´´´´´´

---
+++ AtelieDosPontinhos.Desktop/DTOs/EnderecoDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtelieDosPontinhos.Desktop.DTOs
+{
+    public class EnderecoDto
+    {
+        public int Id { get; set; }
+        public string CEP { get; set; } = string.Empty;
+        public int NUMERO { get; set; }
+        public string Estado { get; set; } = string.Empty;
+        public string Referencial { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+    }
+
+    public class CreateEnderecoDto
+    {
+
+        public string CEP { get; set; } = string.Empty;
+        public int NUMERO { get; set; }
+        public string Estado { get; set; } = string.Empty;
+        public string Referencial { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+    }
+
+    public class UpdateEnderecoDto
+    {
+
+        public string CEP { get; set; } = string.Empty;
+        public int NUMERO { get; set; }
+        public string Estado { get; set; } = string.Empty;
+        public string Referencial { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+    }
+}
