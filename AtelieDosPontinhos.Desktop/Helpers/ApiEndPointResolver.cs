1d3511e41edb246c1b45477336f1324b85706f7f
Jessica <jessicagavazzi2006@gmail.com>
2026-08-24 17:29:03 -0300

    pequena correção na ApiEndPointResolver

--- AtelieDosPontinhos.Desktop/Helpers/ApiEndPointResolver.cs
+++ AtelieDosPontinhos.Desktop/Helpers/ApiEndPointResolver.cs
@@ -66,7 +66,7 @@ namespace AtelieDosPontinhos.Desktop.Helpers
             }
 
             // PROPRIEDADE 2: appsettings.json
-            var fromAppSettings = TryResolveFromLaunchSettings();
+            var fromAppSettings = TryResolveFromAppSettings();
             if (fromAppSettings != null)
             {
                 _resolvedUrl += fromAppSettings;
