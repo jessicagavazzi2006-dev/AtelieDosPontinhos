9a966c884333be401a43c9ee92a26c74906252a2
LUCAS <lucarty79@gmail.com>
2026-09-03 14:23:53 -0300

    pedidos gratis

---
+++ AtelieDosPontinhos.Desktop/DTOs/PedidoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtelieDosPontinhos.Desktop.DTOs
+{
+    public class PedidoItem
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+        public Pedido? Pedido { get; set; }
+
+        public int ProductId { get; set; }
+        public ProductResponseDto? Product { get; set; }
+
+        public int Quantidade { get; set; }
+
+    }
+}
