473a0f7dfbcfc477e18545ea385ec5ace972153f
Jessica <jessicagavazzi2006@gmail.com>
2026-09-10 14:48:06 -0300

    Adicionando AboutForm, e Designer terminado nele

--- AtelieDosPontinhos.Desktop/Properties/Resources.Designer.cs
+++ AtelieDosPontinhos.Desktop/Properties/Resources.Designer.cs
@@ -63,6 +63,56 @@ namespace AtelieDosPontinhos.Desktop.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap foto_2 {
+            get {
+                object obj = ResourceManager.GetObject("foto 2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap foto1 {
+            get {
+                object obj = ResourceManager.GetObject("foto1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap foto3 {
+            get {
+                object obj = ResourceManager.GetObject("foto3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap IMG_20241102_174148 {
+            get {
+                object obj = ResourceManager.GetObject("IMG_20241102_174148", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap IMG_20241102_1741481 {
+            get {
+                object obj = ResourceManager.GetObject("IMG_20241102_1741481", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Logo_Ateliê_dos_pontinhos_diminuída {
             get {
                 object obj = ResourceManager.GetObject("Logo Ateliê dos pontinhos diminuída", resourceCulture);
